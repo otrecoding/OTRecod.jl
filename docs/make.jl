6b257dfe33dcf9ee5b6315b58e3cc9da1ed503dc
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2019-06-27 10:31:38 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,13 +3,12 @@ using OTRecod
 
 makedocs(
     sitename = "OTRecod",
-    format = Documenter.HTML(),
-    modules = [OTRecod]
+    authors  = "Jeremy Omer",
+    format   = Documenter.HTML(),
+    modules  = [OTRecod]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/otrecoding/OTRecod.jl.git",
+ )
